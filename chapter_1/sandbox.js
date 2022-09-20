5b0179d04d938664aa9d3f92a87d326a84d3c71e
Yusuf Habu <yuhyam95@gmail.com>
2022-09-20 14:08:38 +0100

    Pretty much done with String methods

---
+++ chapter_1/sandbox.js
@@ -0,0 +1,17 @@
+let age = 50;
+
+let firstName = "Yusuf";
+let lastName = "Habu";
+let email = "yuhyam95@gmail.com"
+
+let fullName = firstName + " " + lastName;
+
+console.log(fullName[-1])
+
+
+// let result = email.indexOf('a');
+// let result = email.lastIndexOf('a');
+// let result = email.slice(0, 10)
+let result = email.substr(1, 50)
+
+console.log(result)
