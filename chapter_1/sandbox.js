bde885946dc18964908c8c22cd51d426b425e2d5
Yusuf Habu <yuhyam95@gmail.com>
2022-11-08 14:27:48 +0100

    create and remove elements

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -127,17 +127,21 @@
 // console.log(user.login())
 // console.log(user.logBlogs())
 
-// const button = document.querySelector('button');
+const ul = document.querySelector('ul');
+const button = document.querySelector('button');
 
-// button.addEventListener('click', () => {
-//     console.log('you clicked me')
-// })
+button.addEventListener('click', () => {
+    const li = document.createElement('li');
+    li.textContent = 'Something new to do';
+    ul.prepend(li)
+})
 
 const items = document.querySelectorAll('li');
 
 items.forEach( item => {
     item.addEventListener('click', e =>{
         e.target.style.textDecoration = 'line-through';
+        e.target.remove();
     })
 })
 
